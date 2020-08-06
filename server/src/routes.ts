5f1c69c731529197663979af8e0c314c6ef11196
Lucas Tadra Mainginski <lucastadra16@gmail.com>
2020-08-06 02:41:25 -0300

    Proffy API

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import ClassesControllers from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesController = new ClassesControllers();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
